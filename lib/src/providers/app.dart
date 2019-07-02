e1436dfe56765c9a1417a808585b884828fe1ab5
boanergepro <antonycarrizo96@gmail.com>
2019-07-02 10:53:04 -0400

    [MOD]: - Agregue los los atribustos para almacenar en el provider los select.

--- lib/src/providers/app.dart
+++ lib/src/providers/app.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 
 class AppProvider with ChangeNotifier {
   ThemeData _currentTheme;
+  String _languageFrom;
+  String _languageTo;
 
   AppProvider() {
+    _languageFrom = null;
+    _languageTo = null;
     _currentTheme = ThemeData(
       backgroundColor: Colors.grey[300],
       buttonColor: Colors.green,
     );
+
   }
 
   ThemeData get currentTheme => _currentTheme;
+  String get languageFrom => _languageFrom;
+  String get languageTo => _languageTo;
 
   set currentTheme(value) {
     _currentTheme = value != ThemeData.dark() ? ThemeData(
@@ -20,5 +27,16 @@ class AppProvider with ChangeNotifier {
 
     notifyListeners();
   }
+
+  set languageFrom(value) {
+    _languageFrom = value;
+    notifyListeners();
+  }
+
+  set languageTo(value) {
+    _languageTo = value;
+    notifyListeners();
+  }
+
 }
 
