d973f87edd8bc5f28b7e9ea54bcde98eee566cbb
boanergepro <antonycarrizo96@gmail.com>
2019-07-03 13:46:51 -0400

    [MOD]: - Agregue el metodo para traducir.

--- lib/src/utils/translate.dart
+++ lib/src/utils/translate.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:translate_app/src/providers/app.dart';
+import 'package:translator/translator.dart';
 
 class Translate {
-  static exchangeLanguages (BuildContext context) {
+  static exchangeLanguages(BuildContext context) {
     final appState = Provider.of<AppProvider>(context);
 
     String from = appState.languageFrom;
     String to = appState.languageTo;
     appState.languageFrom = to;
     appState.languageTo = from;
+  }
+
+  static translator(BuildContext context) async {
+    final appState = Provider.of<AppProvider>(context);
+    appState.loading = true;
+    GoogleTranslator translator = new GoogleTranslator();
+
+    appState.translatedText = await translator.translate(
+      appState.currentText,
+      from: appState.languageFrom,
+      to: appState.languageTo,
+    );
+    appState.loading = false;
   }
 }
