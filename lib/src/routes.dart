ec57615a0555d1324cbdb315f0d36176dea94f95
boanergepro <antonycarrizo96@gmail.com>
2019-07-04 11:38:23 -0400

    [DEL] - Elimine un archivo que no necesitaba

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:translate_app/src/screens/splash_screen.dart';
 import 'package:translate_app/src/screens/welcome_screen.dart';
 import 'package:translate_app/src/screens/home_screen.dart';
-import 'package:translate_app/src/screens/example.dart';
 
 
 final routes = {
   SplashScreen.routerName : (BuildContext context) => SplashScreen(),
   WelcomeScreen.routerName : (BuildContext context) => WelcomeScreen(),
   HomeScreen.routerName : (BuildContext context) => HomeScreen(),
-  Translate.routerName : (BuildContext context) => Translate()
 };
