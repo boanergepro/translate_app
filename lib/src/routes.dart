742101207acc4fc214830d300d06d39c69aebe32
alirio gutierrez <alirio1925@gmail.com>
2019-07-04 17:46:58 -0400

    [ADD] create item favorite screnn

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -3,11 +3,13 @@ import 'package:translate_app/src/screens/splash_screen.dart';
 import 'package:translate_app/src/screens/welcome_screen.dart';
 import 'package:translate_app/src/screens/home_screen.dart';
 import 'package:translate_app/src/screens/example.dart';
+import 'package:translate_app/src/screens/item_favorite_screen.dart';
 
 
 final routes = {
   SplashScreen.routerName : (BuildContext context) => SplashScreen(),
   WelcomeScreen.routerName : (BuildContext context) => WelcomeScreen(),
   HomeScreen.routerName : (BuildContext context) => HomeScreen(),
-  Translate.routerName : (BuildContext context) => Translate()
+  Translate.routerName : (BuildContext context) => Translate(),
+  ItemFavoriteScreen.routerName : (BuildContext context) => ItemFavoriteScreen(),
 };
