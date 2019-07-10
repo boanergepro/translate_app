c2f7eed46ca186ad063d23b462083023105490da
alirio gutierrez <alirio1925@gmail.com>
2019-07-10 15:06:52 -0400

    [ADD] method delete and delete all

--- lib/src/screens/favorite_screen.dart
+++ lib/src/screens/favorite_screen.dart
@@ -15,6 +15,34 @@ class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppProvider>(context);
+    void _showAlert(String value) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return new AlertDialog(
+              content: new Text(value),
+              actions: <Widget>[
+                new FlatButton(
+                  onPressed: () async {
+                    var snapshot = await Firestore.instance.collection('favorite').getDocuments();
+                    snapshot.documents.forEach((doc) {
+                      doc.reference.delete();
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: new Text('Yes'),
+                ),
+                new FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: new Text('No'),
+                )
+              ],
+            );
+          }
+      );
+    }
 
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
@@ -51,7 +79,12 @@ class FavoriteScreen extends StatelessWidget {
                     fontSize: 15.0,
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () async{
+                  var query = await Firestore.instance.collection('favorite').getDocuments();
+                  if(query.documents.isNotEmpty){
+                    _showAlert('Are you sure to eliminate?');
+                  }
+                },
               ),
             ),
           ),
@@ -59,85 +92,99 @@ class FavoriteScreen extends StatelessWidget {
             flex: 25,
             child: Container(
               padding: EdgeInsets.only(right: 15, left: 15),
-              child: FutureBuilder(
-                future:
-                    Firestore.instance.collection('favorite').getDocuments(),
+              child: StreamBuilder(
+                stream: Firestore.instance.collection('favorite').snapshots(),
                 builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return Center(
                       child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.greenAccent[200])
-                      ),
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                              Colors.greenAccent[200])),
                     );
                   } else {
                     List<Favorite> _favorites = snapshot.data.documents
                         .map((doc) => Favorite.fromSnapshot(doc))
                         .toList();
-                    return ListView.builder(
-                      itemCount: _favorites.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Slidable(
-                          actionPane: SlidableDrawerActionPane(),
-                          actionExtentRatio: 0.25,
-                          child: Container(
-                            child: Card(
-                              child: Container(
-                                padding: EdgeInsets.all(10),
-                                height: 120,
-                                child: Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      flex: 1,
-                                      child: Icon(
-                                        Icons.star,
-                                        color: Colors.greenAccent[200],
+                    if (_favorites.isEmpty) {
+                      return Center(
+                        child: Text(
+                          'Data not available',
+                          style: TextStyle(
+                            fontFamily: 'Lato',
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                        itemCount: _favorites.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Slidable(
+                            actionPane: SlidableDrawerActionPane(),
+                            actionExtentRatio: 0.25,
+                            child: Container(
+                              child: Card(
+                                child: Container(
+                                  padding: EdgeInsets.all(10),
+                                  height: 120,
+                                  child: Row(
+                                    children: <Widget>[
+                                      Expanded(
+                                        flex: 1,
+                                        child: Icon(
+                                          Icons.star,
+                                          color: Colors.greenAccent[200],
+                                        ),
                                       ),
-                                    ),
-                                    Expanded(
-                                      flex: 9,
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            _favorites[index].translateFrom,
-                                            style: TextStyle(
-                                              fontFamily: 'Lato',
-                                              fontSize: 12.0,
+                                      Expanded(
+                                        flex: 9,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(
+                                              _favorites[index].translateFrom,
+                                              style: TextStyle(
+                                                fontFamily: 'Lato',
+                                                fontSize: 12.0,
+                                              ),
                                             ),
-                                          ),
-                                          Text(
-                                            _favorites[index].translateTo,
-                                            style: TextStyle(
-                                              fontFamily: 'Lato',
-                                              fontSize: 12.0,
+                                            Text(
+                                              _favorites[index].translateTo,
+                                              style: TextStyle(
+                                                fontFamily: 'Lato',
+                                                fontSize: 12.0,
+                                              ),
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          secondaryActions: <Widget>[
-                            IconSlideAction(
-                              iconWidget: Icon(
-                                Icons.delete,
-                                color: Colors.redAccent,
-                                size: 30,
+                            secondaryActions: <Widget>[
+                              IconSlideAction(
+                                iconWidget: Icon(
+                                  Icons.delete,
+                                  color: Colors.redAccent,
+                                  size: 30,
+                                ),
+                                closeOnTap: true,
+                                color: Theme.of(context).backgroundColor,
+                                onTap: () {
+                                  Firestore.instance
+                                      .collection('favorite')
+                                      .document('${_favorites[index].id}')
+                                      .delete();
+                                },
                               ),
-                              closeOnTap: true,
-                              color: Theme.of(context).backgroundColor,
-                              onTap: () {
-                                print('touch');
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
+                            ],
+                          );
+                        },
+                      );
+                    }
                   }
                 },
               ),
